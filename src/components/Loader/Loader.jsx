48625cffeb51adf92bdb078ab9075f501739656a
Altiordev <akbarxon2005@gmail.com>
2023-05-09 19:49:03 +0500

    first commit

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+/** @format */
+import "./loader.scss";
+
+const Loader = () => {
+  return (
+    <div className='modal__loader'>
+      <span className='loader'></span>
+    </div>
+  );
+};
+
+export default Loader;
